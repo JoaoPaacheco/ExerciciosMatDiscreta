99a9ab04ba94daadb446db73845d9b4f3cdc4e9e
Joao Pedro Pacheco <joao_pra@hotmail.com>
2015-11-14 14:42:07 -0300

    Terminado o ex1.c

--- ex1.c
+++ ex1.c
@@ -12,7 +12,28 @@ int main(void)
     ul1.pt1 = 0;
     ul1.pt2 = 0;
     
-    printf("tamanho: %lx.\n",sizeof(ul1.pt1));
+    /*printf("tamanho: %lx.\n",sizeof(ul1.pt1));
     printf("valor: %lx.\n",ul1.pt1);
-    return 0;
+    ul1.pt1--;
+    printf("valor: %lx.\n", ul1.pt1);
+    ul1.pt1++;
+    printf("valor: %lx.\n", ul1.pt1);
+    Esta parte do codigo foi feita para confirmar que a variavel tem
+    o tamanho de oito bytes, que sua representacao hexadecimal e 0x'acompanha 16 letras/numeros'
+    e a comprovacao do overflow.*/
+
+   while(ul1.pt1 != 0xFFFFFFFFFFFFFFFF && ul1.pt2 != 0xFFFFFFFFFFFFFFFF) /*condicao ira rodar ate que as duas variaveis estejam completamente cheias.*/
+    {
+        if(ul1.pt1 == 0xFFFFFFFFFFFFFFFF)//no momento do overflow o programa adiciona um a segunda metade da variavel.
+        {   
+            printf("Passou.\n");
+            ul1.pt2++;
+            printf("%lu\n.", ul1.pt2);
+        }
+        ul1.pt1++;
+    }
+
+   printf("Criado tipo com capacidade para 16 bytes de armazenamento.\n");
+    
+   return 0;
 }
