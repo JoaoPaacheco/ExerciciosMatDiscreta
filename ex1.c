d9bf7b38b2efb7b908b43970e9d1e04b42f59b8f
Joao Pedro Pacheco <joao_pra@hotmail.com>
2015-11-14 03:39:04 -0300

    Testes no ex1.c e adicao do arquivo errors.err

--- ex1.c
+++ ex1.c
@@ -4,9 +4,15 @@ int main(void)
 {
     typedef struct
     {
-        unsigned long pt1 = 0; /*primeira parte da variavel maior*/
-        unsigned long pt2 = 0; /*segunda parte da variavel maior*/
+        unsigned long pt1; /*primeira parte da variavel maior*/
+        unsigned long pt2; /*segunda parte da variavel maior*/
     }ullong;
 
     ullong ul1;
+    ul1.pt1 = 0;
+    ul1.pt2 = 0;
+    
+    printf("tamanho: %lx.\n",sizeof(ul1.pt1));
+    printf("valor: %lx.\n",ul1.pt1);
+    return 0;
 }
