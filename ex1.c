18cf31311e2670137d07648ccb1580cc543e529a
Joao Pedro Pacheco <joao_pra@hotmail.com>
2015-11-14 02:41:35 -0300

    Adicionado makefile no develop.

--- ex1.c
+++ ex1.c
@@ -4,8 +4,8 @@ int main(void)
 {
     typedef struct
     {
-        unsigned long pt1 = 0;
-        unsigned long pt2 = 0;
+        unsigned long pt1 = 0; /*primeira parte da variavel maior*/
+        unsigned long pt2 = 0; /*segunda parte da variavel maior*/
     }ullong;
 
     ullong ul1;
