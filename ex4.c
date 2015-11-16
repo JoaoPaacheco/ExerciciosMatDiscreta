20fc2dd0aa5c9c91b43e863d3e08699a0134126a
Joao Pedro Pacheco <joao_pra@hotmail.com>
2015-11-16 03:45:06 -0300

    Testes no ex4.c, estudar strings.

--- ex4.c
+++ ex4.c
@@ -1,6 +1,32 @@
 #include<stdio.h>
+#include<time.h>
+
+#define QNTPESSOAS 10 //define a quantidade de pessoas da tabela
+#define TAMNOME 21 //define o tamanho maximo dos nomes
+#define TAMCEL 12 //define o tamanho dos telefenos celulares de acordo com o padrao do DDD 81 ex: xxxxx-xxxx
 
 int main(void)
 {
+    char listanomes[QNTPESSOAS][TAMNOME];
+    char listanumeros[QNTPESSOAS][TAMCEL];
+    int i, s, aux;
+    //preencher(listanomes[QNTPESSOAS][TAMNOME]);
+
+    srand(time(NULL));
+
+    for(i = 0 ; i <= QNTPESSOAS; i++)
+        for(s = 0 ; s <= TAMNOME; s++)
+            listanomes[i][s] = '\0';
+
+    for(i = 0; i <= QNTPESSOAS; i++)
+    {
+        for(s = 0; s <= TAMNOME - 1; s++)
+        {
+            aux = (66 + rand()%(91-66));
+            listanomes[i][s] = (char) aux;
+        }
+        printf("%s.\n",listanomes[i]);
+    }
+
     return 0;
 }
