7f5a6bb6cce93047220d0ecdeaf51bb2992f7833
Joao Pedro Pacheco <joao_pra@hotmail.com>
2015-11-16 13:55:56 -0300

    Algoritmo corrigido e pronto para adicao da busca de nomes.

--- ex4.c
+++ ex4.c
@@ -2,31 +2,62 @@
 #include<time.h>
 
 #define QNTPESSOAS 10 //define a quantidade de pessoas da tabela
-#define TAMNOME 21 //define o tamanho maximo dos nomes
-#define TAMCEL 12 //define o tamanho dos telefenos celulares de acordo com o padrao do DDD 81 ex: xxxxx-xxxx
+#define TAMNOME 11 //define o tamanho maximo dos nomes
+#define TAMCEL 11 //define o tamanho dos telefenos celulares de acordo com o padrao do DDD 81 ex: xxxxx-xxxx
 
 int main(void)
 {
     char listanomes[QNTPESSOAS][TAMNOME];
     char listanumeros[QNTPESSOAS][TAMCEL];
     int i, s, aux;
-    //preencher(listanomes[QNTPESSOAS][TAMNOME]);
 
     srand(time(NULL));
 
-    for(i = 0 ; i <= QNTPESSOAS; i++)
-        for(s = 0 ; s <= TAMNOME; s++)
-            listanomes[i][s] = '\0';
+    printf("Segue a lista de nomes e numeros.");
 
-    for(i = 0; i <= QNTPESSOAS; i++)
+    for(i = 0 ; i <= QNTPESSOAS; i++)
     {
-        for(s = 0; s <= TAMNOME - 1; s++)
-        {
-            aux = (66 + rand()%(91-66));
-            listanomes[i][s] = (char) aux;
-        }
-        printf("%s.\n",listanomes[i]);
+        listanomes[i][TAMNOME - 1] = '\0';
+        listanumeros[i][TAMCEL - 1] = '\0';
     }
+
+    for(i = 0; i <= QNTPESSOAS - 1; i++)
+    {
+        for(s = 0; s <= TAMNOME - 2; s++)
+        {
+            if(s != 0)
+            {
+                aux = (97 + rand()%(123-97));
+                listanomes[i][s] = (char) aux;
+            }
+            else
+            {
+                aux = (65 + rand()%(91-65));
+                listanomes[i][s] = (char) aux;
+            }
+        }
+        printf("Nome: %s.",listanomes[i]);
+        for(s = 0; s <= TAMCEL - 2; s++)
+        {
+            if(s == 0)
+                listanumeros[i][s] = '9';
+           else
+           {
+                if(s == 5)
+                {
+                listanumeros[i][s] = '-';
+                }
+                else
+                {
+                    aux = (48 + rand()%10);
+                    listanumeros[i][s] = (char) aux;
+                }
+           }
+        }
+        printf(" Numero: %s.\n", listanumeros[i]);
+    }
+
+    printf("Escolha um nome da lista para confirmar o numero do celular:\n");
 
     return 0;
 }
