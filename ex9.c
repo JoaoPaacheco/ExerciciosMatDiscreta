91e2e85c997a5c00f6f144babb763a8344c64966
Joao Pedro Pacheco <joao_pra@hotmail.com>
2015-11-17 15:30:37 -0300

    Funcao de multiplicacao de matrizes criada e polida.

--- ex9.c
+++ ex9.c
@@ -1,4 +1,31 @@
 #include<stdio.h>
+#include<pthread.h>
+
+#define max 1000
+
+int result[max][max], mat1[max][max], mat2[max][max];
+
+void* mult(void *z, void *y)
+{
+    int *x = (int *) z;
+    int limitesup = *x;
+    int *f = (int *) y;
+    int limiteinf = *f;
+    int i, s, a;
+    for(i = limiteinf; i < limitesup; i++)
+    {
+        for (s = 0; s < max ; s++)
+        {
+            a = 0;
+            while (a < max)
+            {
+                result[i][s] += mat1[i][a] * mat2[a][s];
+                a++;
+            }
+        }
+    }
+    pthread_exit(0);
+}
 
 int main(void)
 {
